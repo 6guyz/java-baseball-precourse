e604a65633814b50b9979a7f238582ebe7654569
rhyuys115 <ryeongseok1125@gmail.com>
2021-11-24 00:50:45 +0900

    feat: 입력 유효성 검사 추가

--- src/main/java/utils/GameUtils.java
+++ src/main/java/utils/GameUtils.java
@@ -7,12 +7,24 @@ public class GameUtils {
         String answer = makeAnswer();
         System.out.printf("숫자를 입력해주세요 : ");
         while (true) {
-            scanner.nextLine();
+            String submitNumber = scanner.nextLine();
+            isValidInput(submitNumber);
         }
     }
 
     private static String makeAnswer() {
         Integer intAnswer = 100*RandomUtils.nextInt(1,9) + 10*RandomUtils.nextInt(1,9) + RandomUtils.nextInt(1,9);
         return intAnswer.toString();
+    }
+
+    private static void isValidInput(String input) {
+        if (input.length()>3 || input.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
     }
 }

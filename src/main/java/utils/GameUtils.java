7ca6c72164791928f8b51b6831462149ea0bdd4a
rhyuys115 <ryeongseok1125@gmail.com>
2021-11-25 23:46:49 +0900

    feat: 정답 확인 및 정답 후처리 로직 추가

--- src/main/java/utils/GameUtils.java
+++ src/main/java/utils/GameUtils.java
@@ -6,8 +6,8 @@ public class GameUtils {
     private static boolean isEnd = false;
     public static void startGame(Scanner scanner) {
         String answer = makeAnswer();
-        while (isEnd) {
-            progressTurn(scanner);
+        while ( !isEnd ) {
+            progressTurn(scanner, answer);
         }
     }
 
@@ -27,9 +27,21 @@ public class GameUtils {
         }
     }
 
-    private static void progressTurn(Scanner scanner) {
+    private static void progressTurn(Scanner scanner, String answer) {
         System.out.printf("숫자를 입력해주세요 : ");
         String submitNumber = scanner.nextLine();
         isValidInput(submitNumber);
+        if (isAnswer(submitNumber, answer)) {
+            isEnd = true;
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+    }
+
+    private static boolean isAnswer(String submitNumber, String answer) {
+        if (answer.equals(submitNumber)) {
+            return true;
+        }
+        return false;
     }
 }

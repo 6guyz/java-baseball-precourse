91713e5991b56b7eac60e49a2c7d09fca88f6f52
sungyujeon <sungyujeon91@gmail.com>
2021-12-11 13:06:09 +0900

    feat: 값 비교 함수 추가

--- src/main/java/utils/GameUtils.java
+++ src/main/java/utils/GameUtils.java
@@ -1,7 +1,7 @@
 package utils;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameUtils {
 
@@ -26,7 +26,46 @@ public class GameUtils {
 
     // targetNumber and userNumber 비교
     public boolean compareTargetAndUserNumber(String targetNumber, String userNumber) {
+        if (targetNumber.equals(userNumber)) return true;
 
-        return true;
+        String[] targetNumberArray = targetNumber.split("");
+        Map<String, Integer> map = createTargetNumberMap(targetNumberArray);
+        int strikes = 0;
+        int balls = 0;
+
+        for (int i = 0; i < targetNumber.length(); i++ ) {
+            String targetNum = Character.toString(targetNumber.charAt(i));
+            String userNum = Character.toString(userNumber.charAt(i));
+
+            if (targetNum.equals(userNum)) {
+                strikes++;
+                map.put(targetNum, map.get(targetNum) - 1);
+            } else {
+                if (map.containsKey(userNum) && map.get(userNum) != 0) {
+                    map.put(userNum, map.get(userNum) - 1);
+                    balls++;
+                }
+            }
+        }
+
+        if (balls != 0 && strikes != 0) {
+            System.out.printf("%d볼 %d스트라이크\n", balls, strikes);
+        } else if (balls != 0 && strikes == 0) {
+            System.out.printf("%d볼\n", balls);
+        } else if (balls == 0 && strikes != 0) {
+            System.out.printf("%d스트라이크\n", strikes);
+        }
+        return false;
+    }
+    private Map<String, Integer> createTargetNumberMap(String[] targetNumberArray) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (String num : targetNumberArray) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        return map;
     }
 }
